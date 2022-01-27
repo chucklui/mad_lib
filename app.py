c874163c06d04fef3106a4502dd465b0b40c9662
hectorchu~ <hector.chu828@gmail.com>
2022-01-26 17:59:24 -0800

    Completed mad libs

--- app.py
+++ app.py
@@ -7,3 +7,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get("/")
+def ask_questions():
+    words = silly_story.prompts
+    print(words)
+    return render_template("questions.html", prompts=words)
+
+@app.get("/story")
+def add_story():
+    completed_story = silly_story.generate(request.args)
+    return render_template("story.html", completed_story=completed_story)
